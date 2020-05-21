440777ecea5a6e58c0aac31e31c132334a134bba
Pavel-coder <57906848+Pavel-coder@users.noreply.github.com>
2020-05-21 18:18:33 +0300

    render links with map

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,29 +1,27 @@
 import React from 'react';
 import Logo from '../../assets/react.png';
-import { UserInfo } from '../userInfo/UserInfo';
+// import { UserInfo } from '../userInfo/UserInfo';
 import { links } from '../../constants';
 
 import './Header.scss';
 
-export const Header = (props) => {
+export const Header = () => {
   return (
     <header className="may-header navbar">
       <img className="may-header-logo" alt="app logo" src={Logo} />
 
       <div className="may-header-links-wrapper">
-        <div className="nav-item">
-          <a href={links[0].url}  className="may-header-links-wrapper-link nav-link">{links[0].name}</a>
-        </div>
-        <div className="nav-item">
-          <a href={links[1].url}  className="may-header-links-wrapper-link nav-link">{links[1].name}</a>
-        </div>
-        <div className="nav-item">
-          <a href={links[2].url} className="may-header-links-wrapper-link nav-link">{links[2].name}</a>
-        </div>
+            {links.map((item,index) =>{
+                return (
+                    <div key={index} className="nav-item">
+                    <a href={item.url}  className="may-header-links-wrapper-link nav-link">{item.name}</a>
+                        </div>
+                )
+            })}
       </div>
 
-      <UserInfo />
+    {/*  /!*<UserInfo />*!/*/}
 
     </header>
   );
-};
+      };
